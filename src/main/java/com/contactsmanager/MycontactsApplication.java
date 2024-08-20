abd4f8779f2bb5cfa8151456699e322847042fc8
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2024-08-20 18:20:00 -0400

    Initial setup

---
+++ src/main/java/com/contactsmanager/MycontactsApplication.java
@@ -0,0 +1,13 @@
+package com.contactsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycontactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycontactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/contactsmanager/MycontactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
