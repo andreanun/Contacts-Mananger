20369d5351091d26190a3626b41ee0fb01227999
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2024-08-21 18:41:43 -0400

    Refactored main app name and added contact entity

--- src/main/java/com/contactsmanager/MyContactsApplication.java
+++ src/main/java/com/contactsmanager/MyContactsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MycontactsApplication {
+public class MyContactsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MycontactsApplication.class, args);
+        SpringApplication.run(MyContactsApplication.class, args);
     }
 
 }
