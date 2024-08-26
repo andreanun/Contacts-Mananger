12f3d7148fd8fb9818a214e4108e9bcc74b118d3
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2024-08-26 17:12:09 -0400

    Added contact service class with url path constant

---
+++ src/main/java/com/contactsmanager/ContactRepository.java
@@ -0,0 +1,20 @@
+package com.contactsmanager;
+
+import com.contactsmanager.contact.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, String> {
+    Optional<Contact> findById (String id);
+    Optional<Contact> findByName(String name);
+    Optional<Contact> findByPhone(String phone);
+    Optional<Contact> findByTitle(String id);
+
+
+
+
+
+}
---
+++ src/main/java/com/contactsmanager/ContactService.java
@@ -0,0 +1,74 @@
+package com.contactsmanager;
+
+import com.contactsmanager.contact.Contact;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.contactsmanager.constants.Constant.PHOTO_DIRECTORY;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+
+    private final ContactRepository contactRepository;
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepository.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+    public Contact getContact(String id) {
+        return contactRepository.findById(id).orElseThrow(() -> new RuntimeException("Sorry :(. Contact not found"));
+    }
+    public Contact createContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+    public void deleteContact(Contact contact) {
+
+    }
+    public String uploadPhoto(String id, MultipartFile file) {
+        Contact contact = getContact(id);
+        String photoUrl = photoFunction.apply(id, file);
+        contact.setPhotoUrl(photoUrl);
+        contactRepository.save(contact);
+        return photoUrl;
+    }
+
+    //returns an optional. ex myphoto.png should return .png
+    private final Function<String, String> fileExtension = fileName -> Optional.of(fileName)
+            .filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(fileName.lastIndexOf(".") + 1)).orElse(".png");
+
+    //determines photo url. Takes in id and file name
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String fileName = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            //Path fileStorageLocation = Paths.get("").toAbsolutePath().normalize();
+            if (!Files.exists(fileStorageLocation)) { Files.createDirectories(fileStorageLocation); }
+            Files.copy(image.getInputStream(), fileStorageLocation
+                    .resolve(id + fileExtension.apply(image.getOriginalFilename()) ), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + fileName).toUriString();
+        }catch (Exception exception) {
+            throw new RuntimeException("Ooops, something went wrong :(");
+        }
+    };
+
+}
